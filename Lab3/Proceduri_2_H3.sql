3fadb9ef4506d20b0acf1510b959b720b0b60533
rebeccaichim <147834202+rebeccaichim@users.noreply.github.com>
2024-03-27 01:00:15 +0200

    Add files via upload

---
+++ Lab3/Proceduri_2_H3.sql
@@ -0,0 +1,330 @@
+CREATE TABLE Version (
+    currentVersion INT
+);
+
+
+INSERT INTO Version (currentVersion) VALUES (1);
+
+CREATE TABLE Versionen (
+    VersionNummer INT,
+    Prozedur NVARCHAR(MAX),
+    Parameter1 VARCHAR(MAX),
+    Parameter2 VARCHAR(MAX),
+    Parameter3 VARCHAR(MAX),
+    Parameter4 VARCHAR(MAX)  
+);
+
+
+CREATE OR ALTER PROCEDURE GoToVersion(@changedVersion INT)
+AS
+BEGIN
+    DECLARE @currentVersion INT;
+
+    DECLARE @procedureName VARCHAR(MAX),
+            @TableName VARCHAR(MAX),
+            @ColumnName VARCHAR(MAX),
+            @Parameter1 VARCHAR(MAX),
+            @Parameter2 VARCHAR(MAX),
+            @Parameter3 VARCHAR(MAX),
+            @Parameter4 VARCHAR(MAX);
+
+    SET @currentVersion = (SELECT currentVersion FROM Version);
+
+    IF (@currentVersion > @changedVersion)
+    BEGIN
+        BEGIN TRANSACTION;
+
+        WHILE @currentVersion > @changedVersion
+        BEGIN
+            SELECT @procedureName = v.Prozedur,
+                   @TableName = v.Parameter1,
+                   @ColumnName = v.Parameter2,
+                   @Parameter3 = v.Parameter3,
+                   @Parameter4 = v.Parameter4
+            FROM Versionen v
+            WHERE VersionNummer = @currentVersion;
+
+            PRINT('Rolling back: ' + @procedureName);
+
+            IF (@procedureName = 'CreateTableVersioned')
+                EXEC RollbackCreateTable @TableName;
+            ELSE IF (@procedureName = 'AddForeignKeyConstraintVersioned')
+                EXEC RollbackAddForeignKeyConstraint @TableName, @ColumnName;
+            ELSE IF (@procedureName = 'AddColumnToTableVersioned')
+                EXEC RollbackAddColumnToTable @TableName, @ColumnName;
+            ELSE IF (@procedureName = 'AddDefaultConstraintVersioned')
+                EXEC RollbackAddDefaultConstraint @TableName, @ColumnName;
+			ELSE IF (@procedureName = 'ModifyColumnTypeVersioned')
+                SELECT @Parameter4 = v.Parameter4
+                FROM Versionen v
+                WHERE VersionNummer = @currentVersion;
+
+                EXEC RollbackModifyColumnType @TableName, @ColumnName, @Parameter4;
+
+            SET @currentVersion = @currentVersion - 1;
+
+            UPDATE Version SET currentVersion = @currentVersion;
+        END
+
+        COMMIT;
+    END
+    ELSE
+    BEGIN
+        BEGIN TRANSACTION;
+
+        SET @currentVersion = @currentVersion + 1;
+
+        WHILE @currentVersion < @changedVersion + 1
+        BEGIN
+            SELECT @procedureName = v.Prozedur,
+                   @TableName = v.Parameter1,
+                   @ColumnName = v.Parameter2,
+                   @Parameter3 = v.Parameter3,
+                   @Parameter4 = v.Parameter4
+            FROM Versionen v
+            WHERE v.VersionNummer = @currentVersion;
+
+            PRINT('Applying: ' + @procedureName);
+
+            IF (@procedureName = 'CreateTableVersioned')
+                EXEC CreateTable @TableName, @ColumnName;
+            ELSE IF (@procedureName = 'AddForeignKeyConstraintVersioned')
+                EXEC AddForeignKeyConstraint @TableName, @ColumnName, @Parameter3, @Parameter4;
+            ELSE IF (@procedureName = 'AddColumnToTableVersioned')
+                EXEC AddColumnToTable @TableName, @ColumnName, @Parameter3;
+            ELSE IF (@procedureName = 'AddDefaultConstraintVersioned')
+                EXEC AddDefaultConstraint @TableName, @ColumnName, @Parameter3;
+            ELSE IF (@procedureName = 'ModifyColumnTypeVersioned')
+                EXEC ModifyColumnType @TableName, @ColumnName, @Parameter3;
+
+            SET @currentVersion = @currentVersion + 1;
+
+            UPDATE Version SET currentVersion = @currentVersion;
+        END
+
+        COMMIT;
+    END;
+END;
+
+CREATE OR ALTER PROCEDURE CreateTableVersioned
+    @TableName VARCHAR(50),
+    @ColumnsDefinition VARCHAR(MAX)
+AS 
+BEGIN
+    DECLARE @version AS INT
+    SET @version = (SELECT ISNULL(MAX(VersionNummer), 0) FROM Versionen) + 1
+
+    INSERT INTO Versionen (VersionNummer, Prozedur, Parameter1, Parameter2)
+    VALUES (@version, 'CreateTableVersioned', @TableName, @ColumnsDefinition)
+
+    UPDATE Version
+    SET currentVersion = @version
+
+    PRINT('CREATE TABLE ' + @TableName + ' (' + @ColumnsDefinition + ')')
+    DECLARE @query AS VARCHAR(MAX) = 'CREATE TABLE ' + @TableName + ' (' + @ColumnsDefinition + ')'
+    EXEC(@query)
+END
+GO
+
+
+CREATE OR ALTER PROCEDURE AddForeignKeyConstraintVersioned
+    @TableName VARCHAR(50),
+    @ColumnName VARCHAR(50),
+    @ReferencedTable VARCHAR(50),
+    @ReferencedColumn VARCHAR(50)
+AS
+BEGIN
+DECLARE @version AS int
+SET @version= (SELECT currentVersion FROM Version)+1
+INSERT INTO Versionen(VersionNummer, Prozedur, Parameter1, Parameter2, Parameter3, Parameter4) VALUES (@version, 'AddForeignKeyConstraintVersioned', @TableName , @ColumnName , @ReferencedTable , @ReferencedColumn )
+
+UPDATE Version
+SET currentVersion=currentVersion+1
+DECLARE @query AS VARCHAR(MAX) = 'ALTER TABLE ' + @TableName + ' ADD CONSTRAINT FK_' + @TableName + '_' + @ColumnName +
+               ' FOREIGN KEY (' + @ColumnName + ') REFERENCES ' + @ReferencedTable + '(' + @ReferencedColumn + ')'
+print(@query)
+EXEC(@query)
+END
+GO
+
+CREATE OR ALTER PROCEDURE AddColumnToTableVersioned
+    @TableName VARCHAR(50),
+    @ColumnName VARCHAR(50),
+    @DataType VARCHAR(50)
+AS
+BEGIN
+DECLARE @version AS int
+SET @version= (SELECT currentVersion FROM Version)+1
+INSERT INTO Versionen(VersionNummer, Prozedur, Parameter1, Parameter2, Parameter3) VALUES (@version, 'AddColumnToTableVersioned', @TableName, @ColumnName, @DataType)
+
+UPDATE Version
+SET currentVersion=currentVersion+1
+DECLARE @query AS VARCHAR(MAX) = 'ALTER TABLE ' + @TableName + ' ADD ' + @ColumnName + ' ' + @DataType
+EXEC(@query)
+END
+GO
+
+
+CREATE OR ALTER PROCEDURE AddDefaultConstraintVersioned
+    @TableName VARCHAR(50),
+    @ColumnName VARCHAR(50),
+    @DefaultValue VARCHAR(50)
+AS
+BEGIN
+DECLARE @version AS int
+SET @version= (SELECT currentVersion FROM Version)+1
+INSERT INTO Versionen(VersionNummer, Prozedur, Parameter1, Parameter2, Parameter3) VALUES (@version, 'AddDefaultConstraintVersioned', @TableName, @ColumnName, @DefaultValue)
+
+UPDATE Version
+SET currentVersion=currentVersion+1
+DECLARE @checkedDefaultValue varchar(100)
+IF ISNUMERIC(@defaultValue) = 1
+SET @checkedDefaultValue = @defaultValue
+ELSE
+SET @checkedDefaultValue = '''' + @defaultValue + ''''
+DECLARE @query AS VARCHAR(MAX) =  'ALTER TABLE ' + @TableName + ' ADD CONSTRAINT DF_' + @TableName + '_' + @ColumnName + ' DEFAULT ' + @DefaultValue + ' FOR ' + @ColumnName
+EXEC(@query)
+END
+GO
+
+
+CREATE OR ALTER PROCEDURE ModifyColumnTypeVersioned
+    @TableName VARCHAR(50),
+    @ColumnName VARCHAR(50),
+    @NewDataType VARCHAR(50)AS
+BEGIN
+DECLARE @version AS int
+SET @version= (SELECT currentVersion FROM Version)+1
+
+DECLARE @size AS int
+SET @size = (SELECT CHARACTER_MAXIMUM_LENGTH FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME=@TableName AND COLUMN_NAME=@ColumnName)
+DECLARE @initialType AS VARCHAR(MAX)
+SET @initialType= (SELECT DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME=@TableName AND COLUMN_NAME=@ColumnName)
+
+if @size is not null
+begin
+SET @initialType= @initialType + '(' + cast(@size as varchar(max)) + ')' 
+end
+INSERT INTO Versionen(VersionNummer, Prozedur, Parameter1, Parameter2, Parameter3, Parameter4) VALUES (@version, 'modifyColumnType', @TableName, @ColumnName, @NewDataType, @initialType)
+
+UPDATE Version
+SET currentVersion=currentVersion+1
+DECLARE @query AS VARCHAR(MAX) = 'alter table ' + @TableName + ' ' + 'alter column ' + @ColumnName + ' ' + @NewDataType
+EXEC(@query)
+END
+GO
+
+CREATE OR ALTER PROCEDURE RollbackModifyColumnType
+    @TableName VARCHAR(50),
+    @ColumnName VARCHAR(50),
+    @OldDataType VARCHAR(50)
+AS
+BEGIN
+    DECLARE @query AS VARCHAR(MAX) = 'alter table ' + @TableName + ' ' + 'alter column ' + @ColumnName + ' ' + @OldDataType
+    EXEC(@query)
+END;
+
+
+
+
+DELETE FROM Versionen
+GO
+
+UPDATE Version
+SET currentVersion=0;
+
+SELECT * FROM Versionen
+SELECT * FROM VERSION
+
+EXEC CreateTableVersioned @TableName = 'Eltern_Kinder' , @ColumnsDefinition = 'ID_Eltern INT PRIMARY KEY'
+GO
+
+SELECT * FROM Versionen
+SELECT * FROM VERSION
+
+EXEC AddColumnToTableVersioned @TableName='Eltern_Kinder' , @ColumnName='Age', @DataType='INT'
+GO
+
+SELECT * FROM Versionen
+SELECT * FROM VERSION
+
+EXEC AddColumnToTableVersioned @TableName='Eltern_Kinder' , @ColumnName='Name', @DataType='VARCHAR(50)'
+GO
+
+SELECT * FROM Versionen
+SELECT * FROM VERSION
+
+EXEC ModifyColumnTypeVersioned @TableName='Eltern_Kinder', @ColumnName='Name', @NewDataType='CHAR(20)'
+GO
+
+SELECT * FROM Versionen
+SELECT * FROM VERSION
+
+EXEC AddDefaultConstraintVersioned @TableName='Eltern_Kinder', @ColumnName='Age', @DefaultValue='35'
+GO
+
+SELECT * FROM Versionen
+SELECT * FROM VERSION
+
+EXEC AddDefaultConstraintVersioned @TableName='Eltern_Kinder', @ColumnName='Name', @DefaultValue='''Maria'''
+GO
+
+SELECT * FROM Versionen
+SELECT * FROM VERSION
+
+EXEC CreateTableVersioned @TableName = 'Kinder' , @ColumnsDefinition = 'ID_Kind INT PRIMARY KEY, Name VARCHAR(50)'
+GO
+
+SELECT * FROM Versionen
+SELECT * FROM VERSION
+
+EXEC AddColumnToTableVersioned @TableName='Kinder' , @ColumnName='ID_Eltern', @DataType='INT'
+GO
+
+SELECT * FROM Versionen
+SELECT * FROM VERSION
+
+EXEC AddForeignKeyConstraintVersioned @TableName='Kinder', @ColumnName='ID_Eltern', @ReferencedTable='Eltern_Kinder', @ReferencedColumn='ID_Eltern'
+GO
+
+SELECT * FROM Versionen
+SELECT * FROM VERSION
+
+
+
+EXEC GoToVersion @changedversion=3
+GO
+
+SELECT * FROM VERSION
+
+EXEC GoToVersion @changedversion=9
+GO
+
+SELECT * FROM VERSION
+
+EXEC GoToVersion @changedversion=8
+GO
+
+SELECT * FROM VERSION
+
+
+
+DROP TABLE Eltern_Kinder
+GO
+
+DROP TABLE Kinder
+GO
+
+
+DELETE FROM Versionen
+GO
+
+SELECT * FROM Versionen
+SELECT * FROM VERSION
+
+
+UPDATE Version
+SET currentVersion=0;
+
+SELECT * FROM Versionen
+SELECT * FROM VERSION
---
+++ Lab3/Proceduri_H3.sql
@@ -0,0 +1,200 @@
+--DROP PROCEDURE IF EXISTS CreateTable;
+CREATE PROCEDURE CreateTable
+    @TableName VARCHAR(50),
+    @ColumnsDefinition VARCHAR(MAX)
+AS
+BEGIN
+    DECLARE @SQL VARCHAR(MAX);
+    SET @SQL = 'CREATE TABLE ' + @TableName + ' (' + @ColumnsDefinition + ')';
+    EXEC(@SQL);
+END;
+
+EXEC CreateTable 'Eltern', 'ID_Eltern INT PRIMARY KEY, Name VARCHAR(50), Age INT';
+EXEC CreateTable 'Reservierung', 'Reservierung_ID INT PRIMARY KEY, Name VARCHAR(50), Datum DATE';
+
+
+
+--DROP PROCEDURE IF EXISTS RollbackCreateTable;
+CREATE PROCEDURE RollbackCreateTable
+    @TableName VARCHAR(50)
+AS
+BEGIN
+    DECLARE @SQL VARCHAR(MAX);
+    SET @SQL = 'DROP TABLE ' + @TableName;
+    EXEC(@SQL);
+END;
+
+EXEC RollbackCreateTable 'Eltern';
+EXEC RollbackCreateTable 'Reservierung';
+
+
+
+--DROP PROCEDURE IF EXISTS AddForeignKeyConstraint;
+CREATE PROCEDURE AddForeignKeyConstraint
+    @TableName VARCHAR(50),
+    @ColumnName VARCHAR(50),
+    @ReferencedTable VARCHAR(50),
+    @ReferencedColumn VARCHAR(50)
+AS
+BEGIN
+    DECLARE @SQL VARCHAR(MAX);
+    SET @SQL = 'ALTER TABLE ' + @TableName + ' ADD CONSTRAINT FK_' + @TableName + '_' + @ColumnName +
+               ' FOREIGN KEY (' + @ColumnName + ') REFERENCES ' + @ReferencedTable + '(' + @ReferencedColumn + ')';
+    EXEC(@SQL);
+END;
+
+EXEC CreateTable 'Eltern_Kinder', 'ID_Eltern INT PRIMARY KEY, Name VARCHAR(50), Age INT';
+EXEC CreateTable 'Kinder', 'ID_Kind INT PRIMARY KEY, Name VARCHAR(50), ID_Eltern INT';
+EXEC AddForeignKeyConstraint 'Kinder', 'ID_Eltern', 'Eltern_Kinder', 'ID_Eltern';
+
+
+
+--DROP PROCEDURE IF EXISTS RollbackAddForeignKeyConstraint;
+CREATE PROCEDURE RollbackAddForeignKeyConstraint
+    @TableName VARCHAR(50),
+    @ColumnName VARCHAR(50)
+
+AS
+BEGIN
+    DECLARE @sql VARCHAR(MAX);
+    SET @sql = 'ALTER TABLE ' + @TableName + ' DROP CONSTRAINT FK_' + @TableName + '_' + @ColumnName;
+    EXEC (@sql);
+END
+
+EXEC RollbackAddForeignKeyConstraint 'Kinder', 'ID_Eltern' ;
+
+EXEC RollbackCreateTable 'Kinder';
+EXEC RollbackCreateTable 'Eltern_Kinder';
+
+
+
+--DROP PROCEDURE IF EXISTS AddColumnToTable;
+CREATE PROCEDURE AddColumnToTable
+    @TableName VARCHAR(50),
+    @ColumnName VARCHAR(50),
+    @DataType VARCHAR(50)
+AS
+BEGIN
+    DECLARE @SQL NVARCHAR(MAX);
+    SET @SQL = 'ALTER TABLE ' + @TableName + ' ADD ' + @ColumnName + ' ' + @DataType;
+    EXEC(@SQL);
+END;
+
+EXEC CreateTable 'Eltern_Kinder', 'ID_Eltern INT PRIMARY KEY, Name VARCHAR(50)';
+EXEC AddColumnToTable 'Eltern_Kinder', 'Age', 'INT';
+
+
+--DROP PROCEDURE IF EXISTS RollbackAddColumnToTable;
+CREATE PROCEDURE RollbackAddColumnToTable
+    @TableName VARCHAR(50),
+    @ColumnName VARCHAR(50)
+AS
+BEGIN
+    DECLARE @SQL VARCHAR(MAX);
+    SET @SQL = 'ALTER TABLE ' + @TableName + ' DROP COLUMN ' + @ColumnName;
+    EXEC(@SQL);
+END;
+
+EXEC RollbackAddColumnToTable 'Eltern_Kinder', 'Age';
+EXEC RollbackCreateTable 'Eltern_Kinder';
+
+
+--DROP PROCEDURE IF EXISTS AddDefaultConstraint;
+CREATE PROCEDURE AddDefaultConstraint
+    @TableName VARCHAR(50),
+    @ColumnName VARCHAR(50),
+    @DefaultValue VARCHAR(50)
+AS
+BEGIN
+    DECLARE @SQL VARCHAR(MAX);
+    SET @SQL = 'ALTER TABLE ' + @TableName + ' ADD CONSTRAINT DF_' + @TableName + '_' + @ColumnName + ' DEFAULT ' + @DefaultValue + ' FOR ' + @ColumnName;
+    EXEC(@SQL);
+END;
+
+EXEC CreateTable 'Eltern_Kinder', 'ID_Eltern INT PRIMARY KEY, Name VARCHAR(50), Age INT';
+EXEC AddDefaultConstraint 'Eltern_Kinder', 'Age', '35';
+EXEC AddDefaultConstraint 'Eltern_Kinder', 'Name ', '''Maria''';
+
+--INSERT INTO Eltern_Kinder (ID_Eltern) VALUES (2);
+
+
+
+--DROP PROCEDURE IF EXISTS RollbackAddDefaultConstraint;
+CREATE PROCEDURE RollbackAddDefaultConstraint
+    @TableName VARCHAR(50),
+    @ColumnName VARCHAR(50)
+AS
+BEGIN
+    DECLARE @SQL VARCHAR(MAX);
+    SET @SQL = 'ALTER TABLE ' + @TableName + ' DROP CONSTRAINT DF_' + @TableName + '_' + @ColumnName;
+    EXEC(@SQL);
+END;
+
+EXEC RollbackAddDefaultConstraint 'Eltern_Kinder', 'Age';
+EXEC RollbackAddDefaultConstraint 'Eltern_Kinder', 'Name';
+EXEC RollbackCreateTable 'Eltern_Kinder';
+
+--DROP PROCEDURE IF EXISTS RollbackModifyColumnType;
+CREATE PROCEDURE ModifyColumnType
+    @TableName VARCHAR(50),
+    @ColumnName VARCHAR(50),
+    @NewDataType VARCHAR(50)
+AS
+BEGIN
+    DECLARE @SQL VARCHAR(MAX);
+    SET @SQL = 'ALTER TABLE ' + @TableName + ' ALTER COLUMN ' + @ColumnName + ' ' + @NewDataType;
+    EXEC(@SQL);
+END;
+
+
+
+
+--CreateTable
+EXEC CreateTable 'Eltern_Kinder', 'ID_Eltern INT PRIMARY KEY, Name VARCHAR(50)';
+EXEC CreateTable 'Reservierung', 'Reservierung_ID INT PRIMARY KEY, Name VARCHAR(50), Datum DATE';
+--RollbackCreateTable
+EXEC RollbackCreateTable 'Eltern';
+EXEC RollbackCreateTable 'Reservierung';
+--AddForeignKeyConstraint
+EXEC CreateTable 'Eltern_Kinder', 'ID_Eltern INT PRIMARY KEY, Name VARCHAR(50), Age INT';
+EXEC CreateTable 'Kinder', 'ID_Kind INT PRIMARY KEY, Name VARCHAR(50), ID_Eltern INT';
+EXEC AddForeignKeyConstraint 'Kinder', 'ID_Eltern', 'Eltern_Kinder', 'ID_Eltern';
+--RollbackAddForeignKeyConstraint
+EXEC RollbackCreateTable 'Kinder';
+EXEC RollbackCreateTable 'Eltern_Kinder';
+--AddColumnToTable
+EXEC CreateTable 'Eltern_Kinder', 'ID_Eltern INT PRIMARY KEY, Name VARCHAR(50)';
+EXEC AddColumnToTable 'Eltern_Kinder', 'Age', 'INT';
+--RollbackAddColumnToTable
+EXEC RollbackAddColumnToTable 'Eltern_Kinder', 'Age';
+EXEC RollbackCreateTable 'Eltern_Kinder';
+--AddDefaultConstraint
+EXEC CreateTable 'Eltern_Kinder', 'ID_Eltern INT PRIMARY KEY, Name VARCHAR(50), Age INT';
+EXEC AddDefaultConstraint 'Eltern_Kinder', 'Age', '35';
+EXEC AddDefaultConstraint 'Eltern_Kinder', 'Name ', '''Maria''';
+--INSERT INTO Eltern_Kinder (ID_Eltern) VALUES (1);
+--RollbackAddDefaultConstraint
+EXEC RollbackAddDefaultConstraint 'Eltern_Kinder', 'Age';
+EXEC RollbackAddDefaultConstraint 'Eltern_Kinder', 'Name';
+EXEC RollbackCreateTable 'Eltern_Kinder';
+--ModifyColumnType
+EXEC CreateTable 'Eltern_Kinder', 'ID_Eltern INT PRIMARY KEY, Name VARCHAR(50), Age INT';
+EXEC ModifyColumnType 'Eltern_Kinder', 'Age', 'VARCHAR(50)';
+
+
+EXEC CreateTable 'Eltern_Kinder', 'ID_Eltern INT PRIMARY KEY, Name VARCHAR(50)';
+EXEC AddColumnToTable 'Eltern_Kinder', 'Age', 'INT';
+EXEC ModifyColumnType 'Eltern_Kinder', 'Age', 'VARCHAR(50)';
+EXEC CreateTable 'Kinder', 'ID_Kind INT PRIMARY KEY, Name VARCHAR(50), ID_Eltern INT';
+EXEC AddForeignKeyConstraint 'Kinder', 'ID_Eltern', 'Eltern_Kinder', 'ID_Eltern';
+EXEC AddDefaultConstraint 'Eltern_Kinder', 'Age', '35';
+EXEC AddDefaultConstraint 'Eltern_Kinder', 'Name ', '''Maria''';
+
+
+
+
+
+
+
+
+
